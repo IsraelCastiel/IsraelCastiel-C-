61a72f414961ac9793d01c0e922b6fec00844b9c
Israel <israelcastiel@gmail.com>
2021-12-23 15:29:24 -0400

    Inicio

---
+++ repos/analise/analise/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using analise.Models;
+
+namespace analise.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> List = new List<Department>();
+            List.Add(new Department { id = 1, Name = "Exames" });
+            List.Add(new Department { id = 2, Name = "Farmacias" });
+            List.Add(new Department { id = 3, Name = "Vacinas" });
+            return View(List);
+        }
+    }
+}
