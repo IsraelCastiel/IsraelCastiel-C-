73ba4892e365467cc47b4e9e67fd8f6741df280d
Israel <israelcastiel@gmail.com>
2021-12-23 16:53:43 -0400

    Departments

---
+++ repos/analise/analise/Data/analiseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using analise.Models;
+
+namespace analise.Data
+{
+    public class analiseContext : DbContext
+    {
+        public analiseContext (DbContextOptions<analiseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<analise.Models.Department> Department { get; set; }
+    }
+}
